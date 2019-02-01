fb8dec8106a5b0c6e3c9e9442cd4b2a1264667c3
alfonsotech <alfonsotech@nym.hush.com>
2019-02-01 00:55:01 -0400

    Adds functionality to click on cat thumb to display full image

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('inside js');
+$(function() {
+  const hero = $('.hero img');
+  const thumbs = $('.thumbnails');
+  thumbs.on('click', 'img', function(event) {
+    let image = this.src;
+    hero.attr('src', image).attr('alt', this.alt);
+  });
+  thumbs.on('keydown', 'a', function(event) {
+    console.log('event.keyCode', event.keyCode);
+    if(event.keyCode === 13) {
+      console.log($(this).children());
+      let image = $(this).children();
+      hero.attr('src', image[0].src).attr('alt', image[0].alt);
+    }
+  });
+});
